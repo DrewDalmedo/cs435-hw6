7645b9710b605f37d84488e45aabf68c924c0ce9
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-12-19 14:59:42 -0500

    implement heap

--- main.py
+++ main.py
@@ -21,8 +21,82 @@ def frequency_table(st):
 - Input:    A string st (max len 256)
 - Output:   Print each character along with its Huffman code
 '''
+class Node:
+    def __init__(self, char, freq, left=None, right=None):
+        self.char = char
+        self.freq = freq
+
+        self.left = left
+        self.right = right
+
+class Heap:
+    def __init__(self):
+        self.heap_ = []
+
+    def heapify_up(self, idx):
+        while idx > 0:
+            parent = (idx - 1) // 2
+            
+            if self.heap_[idx].freq < self.heap_[parent].freq:
+                self.heap_[idx], self.heap_[parent] = self.heap_[parent], self.heap_[idx]
+                idx = parent
+            else:
+                break
+
+    def heapify_down(self, idx):
+        size = len(self.heap_)
+
+        while True:
+            left = 2 * idx + 1
+            right = 2 * idx + 2
+            smallest = idx
+
+            if left < size and self.heap_[left].freq < self.heap_[smallest].freq:
+                smallest = left
+
+            if right < size and self.heap_[right].freq < self.heap_[smallest].freq:
+                smallest = right
+
+            if smallest != idx:
+                self.heap_[idx], self.heap[smallest] = self.heap_[smallest], self.heap_[idx]
+                idx = smallest
+            else:
+                break
+
+    def push(self, node):
+        self.heap_.append(node)
+        self.heapify_up(len(self.heap_) - 1)
+
+    def pop(self):
+        if len(self.heap_) == 0:
+            return None
+
+        min_node = self.heap_[0]
+        last = self.heap_.pop()
+
+        self.heap_[0] = last
+        self.heapify_down(0)
+
+        return min_node
+        
+    def __getitem__(self, key):
+        return self.heap_[key]
+
+    def __len__(self):
+        return len(self.heap_)
+
 def Huffman_code(st):
-    pass
+    frequencies = frequency_table(st)
+    heap = Heap()
+
+    for char, freq in frequencies.items():
+        heap.push(heap, Node(char, freq))
+
+    while len(heap) > 1:
+        n1 = heap.pop()
+        n2 = heap.pop()
+
+        # ...
 
 
 '''
